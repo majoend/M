3653dd9cbff51267e83dedb3b34c5caa2121778f
majoend <majojanuary32@gmail.com>
2025-11-16 11:53:50 -0400

    cOMO CREAR UN REPO LOCAL Y REMOTO

--- hello.py
+++ hello.py
@@ -1,3 +1,26 @@
 
 #hi this a new repo
+
+""" Para crear un repositorio de git  
+necesitas tener git instalado en tu computadora.
+crea una carpeta nueva y copias su ruta.
+abre la terminal y escribe:
+cd ruta de la carpeta
+
+git init
+git config --global user.name "tu nombre
+git config --global user.email "tu email
+luego creas un archivo nuevo llamado hello.py o como quieras llamarlo
+escribes el codigo que quieras y guardas el archivo, creas un commit 
+con los siguientes comandos:
+git add .
+git commit -m "primer commit
+Sino funciona vas directo a vs code y le das commit desde ahi.
+
+luego creas un repositorio nuevo en github y copias la url del repositorio
+git remote add origin url del repositorio
+git push -u origin master
+ LISTO  
+"""
+""""""
 print("Hello, World!")
